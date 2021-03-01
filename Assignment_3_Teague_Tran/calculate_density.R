9636e5168103182999b9184169ca170c4cc55742
teaguetran <teaguetran@ucsb.edu>
2021-02-28 19:01:03 -0800

    created new file for assignment 3

---
+++ Assignment_3_Teague_Tran/calculate_density.R
@@ -0,0 +1,6 @@
+# calculate density of unknown liquids
+
+calculate_density <- function(m, V, density_threshold = 1){
+  d = m/V
+  return(d)
+}
--- assignment/calculate_density.R
+++
@@ -1,6 +0,0 @@
-# calculate density of unknown liquids
-
-calculate_density <- function(m, V, density_threshold = 1){
-  d = m/V
-  return(d)
-}
